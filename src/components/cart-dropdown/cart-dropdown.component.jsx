423c4f52b534d8f8c5cd12aff75aef2379216c35
Azamat Rasulov <azamat@Azamats-MacBook-Pro.local>
2023-11-20 12:41:33 +0900

    Unused varaiables removed

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -2,7 +2,7 @@ import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
 
-import { selectCartItems, selectIsCartOpen } from "../../store/cart/cart.selector";
+import { selectCartItems } from "../../store/cart/cart.selector";
 import Button from "../button/button.component";
 import CartItem from "../cart-item/cart-item.component";
 
